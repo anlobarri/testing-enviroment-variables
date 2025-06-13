4b974ddef818c77064dbb89d3473b59ad77a33fd
Angel LB <anlobarri@gmail.com>
2025-06-13 11:53:24 +0200

    testing branch vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-2">
-      <h1>Current Environment:</h1>
+      <h1 className="text-2xl font-bold">Current Environment:</h1>
       <p>{process.env.VERCEL_ENV}</p>
 
 
-      <h2>Vercel URL:</h2>
-      <p>{process.env.VERCEL_URL}</p>
+      <h2 className="text-xl font-semibold">Vercel URL:</h2>
+      <p>{process.env.VERCEL_PROJECT_PRODUCTION_URL}</p>
       
 
 
-      <h2>Current Branch:</h2>
+      <h2 className="text-xl font-semibold">Current Branch:</h2>
       <p>{process.env.VERCEL_BRANCH_URL}</p>
     </div>
   )
